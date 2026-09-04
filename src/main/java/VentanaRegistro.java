9578632342463da070ee6f1a0d81aca1c9935997
AriasLQ <l.arias05@ufromail.cl>
2026-09-04 09:54:34 -0400

    Iteracion02 - Definir estructura de VentanaRegistro

--- src/main/java/VentanaRegistro.java
+++ src/main/java/VentanaRegistro.java
@@ -1,2 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class VentanaRegistro {
+
+
+    public VentanaRegistro() {
+    }
+
+
+    public void mostrarVentana(){
+
+    }
+
+
+    public void registrarNuevoUsuario(){
+
+    }
+
+
+    private void volverAlLogin(){
+
+    }
 }

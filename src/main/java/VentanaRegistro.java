642301ec42f914ffba9eba6dd90b75ff2a3cfa50
AriasLQ <l.arias05@ufromail.cl>
2026-09-10 02:38:48 -0300

    Iteracion02 - Rellenar funcion VentanaRegistro

--- src/main/java/VentanaRegistro.java
+++ src/main/java/VentanaRegistro.java
@@ -2,9 +2,32 @@ import javax.swing.*;
 import java.awt.*;
 
 public class VentanaRegistro {
-
+    private final JFrame frame = new JFrame("Registro - Casino Black Cat");
+    private final JLabel lblNombre = new JLabel("Nombre Completo:");
+    private final JTextField txtNombre = new JTextField(15);
+    private final JLabel lblUsuario = new JLabel("Usuario:");
+    private final JTextField txtUsuario = new JTextField(15);
+    private final JLabel lblClave = new JLabel("Clave:");
+    private final JPasswordField txtClave = new JPasswordField(15);
+    private final JButton btnRegistrar = new JButton("Registrar");
+    private final JButton btnVolver = new JButton("Volver");
 
     public VentanaRegistro() {
+        frame.setSize(300, 250);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        frame.add(lblNombre);
+        frame.add(txtNombre);
+        frame.add(lblUsuario);
+        frame.add(txtUsuario);
+        frame.add(lblClave);
+        frame.add(txtClave);
+        frame.add(btnRegistrar);
+        frame.add(btnVolver);
+
+        btnRegistrar.addActionListener(e -> registrarNuevoUsuario());
+        btnVolver.addActionListener(e -> volverAlLogin());
     }
 
 
